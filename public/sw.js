ca47210c5d4270580474e70627217f53026fa3c9
LeoCristt <SergeySav009@yandex.ru>
2025-04-21 12:00:33 +0300

    fix

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,31 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'notes-app-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/manifest.json',
+  '/Home.module.css', 
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/screenshots/screenshot-wide.png',
+  '/screenshots/screenshot-mobile.png',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png",
-        "/screenshots/screenshot-wide.png",
-        "/screenshots/screenshot-mobile.png",
-        "/favicon.ico"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
     })
   );
 });
 
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
